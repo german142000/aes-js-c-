215345a67d5814e98a9f53f25294aaad0f508d19
svsp <77344156+german142000@users.noreply.github.com>
2023-12-08 04:18:30 +0500

    Create aes.h

---
+++ header/aes.h
@@ -0,0 +1,8 @@
+#pragma once
+#define EX extern "C" __declspec(dllexport)
+#define CDLL __cdecl
+
+EX bool CDLL encrypt(char* plainText, int size, char* key);
+EX bool CDLL decrypt(char* chiperText, int size, char* key);
+EX char* CDLL getResult(int* size);
+EX char* CDLL getError(int* size);
