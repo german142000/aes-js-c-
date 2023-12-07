ba3320b20f724444fd443ea8369e9b461a75469d
svsp <77344156+german142000@users.noreply.github.com>
2023-12-08 04:22:17 +0500

    Create example.cpp

---
+++ example/example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "aes.h"
+#include <windows.h>
+#pragma comment(lib, "aes.lib")
+
+using namespace std;
+
+int main()
+{
+    encrypt((char*)"Hello world!", 10, (char*)"dgfyrhebsdhehdcf");
+    int ns = 0;
+    char* res = getResult(&ns);
+    std::string enc(res, ns);
+    std::cout << enc << std::endl;
+    decrypt(res, ns - 1, (char*)"dgfyrhebsdhehdcf");
+    char* res2 = getResult(&ns);
+    std::string dec(res2, ns);
+    std::cout << dec << std::endl;
+}
